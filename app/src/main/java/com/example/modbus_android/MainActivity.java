c58fddcda418b9971c2d21944d13a4e8aa0ea080
Mert Talha <merttalhasimsek@gmail.com>
2023-07-21 16:02:54 +0300

    Final Update

--- app/src/main/java/com/example/modbus_android/MainActivity.java
+++ app/src/main/java/com/example/modbus_android/MainActivity.java
@@ -1,222 +1,91 @@
 package com.example.modbus_android;
 
-import static android.content.ContentValues.TAG;
-
 import android.annotation.SuppressLint;
+import android.os.AsyncTask;
 import android.os.Bundle;
-import android.util.Log;
+import android.view.View;
 import android.widget.Button;
 import android.widget.TextView;
-import android.widget.Toast;
 
 import androidx.appcompat.app.AppCompatActivity;
 
-import com.zgkxzx.modbus4And.requset.ModbusParam;
-import com.zgkxzx.modbus4And.requset.ModbusReq;
-import com.zgkxzx.modbus4And.requset.OnRequestBack;
-
-import java.util.Arrays;
-
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.OutputStream;
+import java.net.Socket;
 
 public class MainActivity extends AppCompatActivity {
 
     Button button;
-    @SuppressLint("MissingInflatedId")
+    TextView textView;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         button = findViewById(R.id.button);
-
-
-
-        button.setOnClickListener(view -> {
-            /*GasLevelTask gasLevelTask = new GasLevelTask();
-            gasLevelTask.execute();*/
-
-            ModbusReq.getInstance().setParam(new ModbusParam()
-                            .setHost("192.168.1.5")
-                            .setPort(502)
-                            .setEncapsulated(false)
-                            .setKeepAlive(true)
-                            .setTimeout(5000)
-                            .setRetries(0))
-                    .init(new OnRequestBack<String>() {
-                        @SuppressLint("SetTextI18n")
-                        @Override
-                        public void onSuccess(String s) {
-                            TextView textView = findViewById(R.id.textView3);
-                            textView.setText(s +"success");
-                            readDataFromKBRNDevice();
-                        }
-                        @SuppressLint("SetTextI18n")
-                        @Override
-                        public void onFailed(String s) {
-                            TextView textView = findViewById(R.id.textView3);
-                            textView.setText(s +"failed");
-                            readDataFromKBRNDevice();
-                        }
-                    });
-
-            /*ModbusReq.getInstance().readCoil(new OnRequestBack<boolean[]>() {
-                @SuppressLint("SetTextI18n")
-                @Override
-                public void onSuccess(boolean[] booleen) {
-                    TextView textView = findViewById(R.id.textView4);
-                    textView.setText(Arrays.toString(booleen) +"success");
-                    Log.d(TAG, "readCoil onSuccess " + Arrays.toString(booleen));
-                    Toast.makeText(MainActivity.this, "readCoil onSuccess", Toast.LENGTH_SHORT).show();
-                }
-
-                @SuppressLint("SetTextI18n")
-                @Override
-                public void onFailed(String msg) {
-                    TextView textView = findViewById(R.id.textView4);
-                    textView.setText(msg +"failed");
-                    Log.e(TAG, "readCoil onFailed " + msg);
-                    Toast.makeText(MainActivity.this, "readCoil onFailed", Toast.LENGTH_SHORT).show();
-                }
-            }, 1, 1, 2);
-
-
-            ModbusReq.getInstance().readDiscreteInput(new OnRequestBack<boolean[]>() {
-                @SuppressLint("SetTextI18n")
-                @Override
-                public void onSuccess(boolean[] booleen) {
-                    Log.d(TAG, "readDiscreteInput onSuccess " + Arrays.toString(booleen));
-                    TextView textView = findViewById(R.id.textView);
-                    textView.setText(Arrays.toString(booleen) + "success");
-                    Toast.makeText(MainActivity.this, "readDiscreteInput onSuccess", Toast.LENGTH_SHORT).show();
-                }
-
-                @SuppressLint("SetTextI18n")
-                @Override
-                public void onFailed(String msg) {
-                    Log.e(TAG, "readDiscreteInput onFailed " + msg);
-                    TextView textView = findViewById(R.id.textView);
-                    textView.setText(msg +"failed");
-                    Toast.makeText(MainActivity.this, "readDiscreteInput onFailed", Toast.LENGTH_SHORT).show();
-                }
-            },1,1,5);
-
-            ModbusReq.getInstance().readHoldingRegisters(new OnRequestBack<short[]>() {
-                @SuppressLint("SetTextI18n")
-                @Override
-                public void onSuccess(short[] data) {
-                    TextView textView = findViewById(R.id.textView5);
-                    textView.setText(Arrays.toString(data) +"success");
-                    Log.d(TAG, "readHoldingRegisters onSuccess " + Arrays.toString(data));
-                    Toast.makeText(MainActivity.this, "readHoldingRegisters onSuccess", Toast.LENGTH_SHORT).show();
-                }
-
-                @SuppressLint("SetTextI18n")
-                @Override
-                public void onFailed(String msg) {
-                    TextView textView = findViewById(R.id.textView5);
-                    textView.setText(msg +"failed");
-                    Log.e(TAG, "readHoldingRegisters onFailed " + msg);
-                    Toast.makeText(MainActivity.this, "readHoldingRegisters onFailed", Toast.LENGTH_SHORT).show();
-                }
-            }, 1, 2, 8);*/
-
-
-
+        textView = findViewById(R.id.textView3);
+        byte b = 0x64;
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                new ReceiveDataTask().execute(b);
+            }
         });
     }
 
-    private void readDataFromKBRNDevice() {
-        // Modbus cihazından gelen verileri burada okuyabilir ve kaydedebilirsiniz.
-        // Örneğin:
-        ModbusReq.getInstance().readHoldingRegisters(new OnRequestBack<short[]>() {
-            @Override
-            public void onSuccess(short[] data) {
-                // Gelen verileri kaydedin veya işleyin.
-                Log.d(TAG, "Read data from KBRN device: " + Arrays.toString(data));
-                TextView textView = findViewById(R.id.textView);
-                textView.setText(Arrays.toString(data));
+    @SuppressLint("StaticFieldLeak")
+    private class ReceiveDataTask extends AsyncTask<Byte, Void, Integer> {
+        @Override
+        protected Integer doInBackground(Byte... bytes) {
+            byte address = bytes[0];
+            int value = 0;
+
+            byte[] data = {
+                    0x00, 0x01, // transaction identifier
+                    0x00, 0x00, // Protocol Identifier (modbus)
+                    0x00, 0x06, // PDU length
+                    0x11,       // Address (17 decimals)
+                    0x03,       // read register command
+                    0x00, address, // from the entered register
+                    0x00, 0x01  // Only 1 register (2 bytes)
+            };
+
+            byte[] rData = new byte[12];
+            try (Socket socket = new Socket("192.168.1.5", 502)) {
+                if (socket.isConnected()) {
+                    OutputStream outputStream = socket.getOutputStream();
+                    outputStream.write(data);
+
+                    InputStream inputStream = socket.getInputStream();
+                    int bytesRead = inputStream.read(rData);
+
+                    if (bytesRead >= 9) {
+                        int pduLength = rData[8] & 0xFF; // Read the PDU length field
+                        int expectedDataLength = pduLength - 2; // Subtract 2 bytes of process identifier from PDU length
+
+                        if (bytesRead >= expectedDataLength + 9) { // Check if the incoming data is greater than or equal to the expected data size
+                            byte[] incomingValue = new byte[]{rData[10], rData[9]}; // Order changed
+                            value = ((incomingValue[1] & 0xFF) << 8) | (incomingValue[0] & 0xFF);
+                            System.out.println("Incoming Value: " + value);
+                        } else {
+                            System.out.println("Missing data received.");
+                        }
+                    } else {
+                        System.out.println("Data could not be read or missing data was received.");
+                    }
+                }
+            } catch (IOException e) {
+                System.out.println("Error: " + e.getMessage());
             }
 
-            @Override
-            public void onFailed(String msg) {
-                Log.e(TAG, "Failed to read data from KBRN device: " + msg);
-                TextView textView = findViewById(R.id.textView);
-                textView.setText(msg);
-            }
-        }, 1, 100, 25);
+            return value;
+        }
+
+        @Override
+        protected void onPostExecute(Integer result) {
+            textView.setText(String.valueOf(result));
+        }
     }
-
-
-    /*@SuppressLint("StaticFieldLeak")
-    public class GasLevelTask extends AsyncTask<Void, Void, Integer> {
-
-        @Override
-        protected Integer doInBackground(Void... voids) {
-            return readGasLevel();
-        }
-
-        @Override
-        protected void onPostExecute(Integer gasLevel) {
-            String mesaj;
-            if (gasLevel >= 0) {
-                mesaj = "Gas Level: " + gasLevel;
-            } else {
-                mesaj = "Failed to get Gas Level";
-            }
-            Toast.makeText(MainActivity.this, mesaj, Toast.LENGTH_SHORT).show();
-        }
-
-    }*/
-
-   /* public int readGasLevel() {
-        String ipAddress = "192.168.1.5";
-        int port = 502;
-        int slaveId = 1;
-        int gasLevelRegister = 40009;
-        int gasLevel;
-        try {
-            InetAddress address = InetAddress.getByName(ipAddress);
-            TCPMasterConnection connection = new TCPMasterConnection(address);
-            connection.setPort(port);
-
-            connection.connect();
-
-            ModbusTCPTransaction transaction = new ModbusTCPTransaction(connection);
-            ReadInputRegistersRequest request = new ReadInputRegistersRequest(gasLevelRegister, 1);
-            request.setUnitID(slaveId);
-            transaction.setRequest(request);
-
-            transaction.execute();
-            ReadInputRegistersResponse response = (ReadInputRegistersResponse) transaction.getResponse();
-
-            gasLevel = response.getRegisterValue(0);
-
-            connection.close();
-
-            return gasLevel;
-        } catch (Exception e) {
-            e.printStackTrace();
-        }
-        ModbusReq.getInstance().setParam(new ModbusParam()
-                        .setHost("192.168.1.5")
-                        .setPort(502)
-                        .setEncapsulated(false)
-                        .setKeepAlive(true)
-                        .setTimeout(2000)
-                        .setRetries(0))
-                .init(new OnRequestBack<String>() {
-                    @Override
-                    public void onSuccess(String s) {
-
-                    }
-
-                    @Override
-                    public void onFailed(String s) {
-
-                    }
-                });
-
-
-
-        return -1;
-    }*/
 }
+
